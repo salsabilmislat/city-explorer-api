36cfa8084802b31a9fd28ae6c7dff31f2544c970
salsabilmislat <salsabilmislat196@gmail.com>
2021-09-07 20:28:59 +0300

    the new lab

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 const express = require('express') // require the express package
 const app = express() // initialize your express app instance
 const cors = require('cors');
-app.use(cors())
+const axios = require('axios');
+app.use(cors());
+
 // a server endpoint 
+require('dotenv').config();
+
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 const weather = require('./data/weather.json')
 app.get('/', // our endpoint name
@@ -20,43 +25,52 @@ class Forecast {
 
 
 
-app.get('/weather', (req, res) => {
+app.get('/weather', async (req, res) => {
     // console.log(request);
 
-    let city_name = req.query.city_name;
+    // let city_name = req.query.city_name;
     let lat = req.query.lat;
     let lon = req.query.lon;
 
+    const weatherBitUrl = 'https://api.weatherbit.io/v2.0/forecast/daily';
     //    res.send(weather[0].lat);
 
-    //  && item.lat === lat && item.lon === lon
+    //  && item.lat === lat && item.lon === lon item.city_name.toLowerCase() === city_name.toLowerCase()
+try{
+    const weatherBitResponse = await axios.get(`${weatherBitUrl}?lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}`); 
+    // console.log(weatherBitResponse);
+    // console.log(weatherBitResponse.data);
+    const returnArray = weatherBitResponse.data.data.map((item) => {
+       
+        return new Forecast(item.datetime, item.weather.description);
+        // if ( item.lat === lat && item.lon === lon) {
 
-    const returnArray = weather.find((item) => {
-
-        return (item.city_name.toLowerCase() === city_name.toLowerCase()) 
+        //     return item;
+        // }
     });
-    if (returnArray){
-        // let arrayOfReturn= returnArray.data;
-        let newArr = returnArray.data.map((item) => {
-            return new Forecast(item.datetime, item.weather.description);
-        })
-        res.json(newArr);
-    }
-    else{
-        res.json('data not found')
-    }
+     console.log(returnArray);
+    // response.json(weatherBitResponse.data);
+   
+    // if (returnArray){
+    //     // let arrayOfReturn= returnArray.data;
+    //     let newArr = returnArray.data.map((item) => {
+    //         return new Forecast(item.datetime, item.weather.description);
+    //     })
+    //     res.json(newArr);
+    // }
+    // else{
+    //     res.json('data not found')
+    // }
+res.json(returnArray);
+}
 
-
-    //     if (returnArray.length) {
-    //         res.json(returnArray);
-    //     } else {
-    //         res.send('no data found :disappointed:')
-    //     }
-    //   } else {
-    //     res.json(weather);
-    //   }
-    // );
+catch (error) {
+    res.json(error);
+  }
+    
 
 });
 // console.log("hello");
-app.listen(3001) // kick start the express server to work
+app.listen(3001,()=>{
+    console.log(" is working from port");
+}) // kick start the express server to work
